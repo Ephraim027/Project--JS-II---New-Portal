dc798d476fd588606689a57b1e9d7ad09ea81815
Ephraim027 <162796399+Ephraim027@users.noreply.github.com>
2026-06-16 23:05:05 +0800

    Add files via upload

---
+++ js/PaymentsRoom/PaymentsRoom7.js
@@ -0,0 +1,24 @@
+
+const params = new URLSearchParams(window.location.search);
+
+const roomName = params.get('room') || 'N/A';
+const days = params.get('days') || '0';
+const total = params.get('total') || '0';
+
+
+// I-display sa HTML
+document.getElementById('BookRoom_title').textContent = roomName;
+document.getElementById('qty').textContent = days;
+document.getElementById('paymentTotal').textContent = `₱ ${total}`;
+
+// Back button
+document.getElementById('backBtn').addEventListener('click', function() {
+  window.location.href = "BookRoom7.html";
+});
+
+// Proceed to Payment
+document.getElementById('payBtn').addEventListener('click', function() {
+  alert(
+    `Proceeding to payment...\n\nRoom: ${roomName}\nDays: ${days}\nTotal: ₱${total}`
+  );
+});
